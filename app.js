709bb630813778faf9b0fb5d3b3eeae331bf9ff3
Freyr Guðjónsson <freyr.gudjonsson@crayon.com>
2024-02-28 12:40:57 +0000

    register servive worker

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // app.js
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  // Update UI to show the install button
+  document.getElementById('installButton').style.display = 'block';
+});
+
 const MyApp = (() => {
     const go = new Go();
 
@@ -43,4 +53,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('callGoButton').addEventListener('click', MyApp.callGoFunctionz);
     document.getElementById('effAroundButton').addEventListener('click', MyApp.effAround);
     document.getElementById('processInputButton').addEventListener('click', MyApp.processInputFunction);
+});
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, err => {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
+document.getElementById('installButton').addEventListener('click', (e) => {
+  // Hide the install button
+  e.target.style.display = 'none';
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+    deferredPrompt = null;
+  });
 });
